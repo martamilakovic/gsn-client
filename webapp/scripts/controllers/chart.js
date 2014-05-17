500be6b9febbca1d8b7c8e23ea5bac1b98aba6c8
Marta Milakovic <marta.milakovic@gmail.com>
2014-05-17 20:44:26 +0200

    Added chart controllers and services.

---
+++ webapp/scripts/controllers/chart.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('gsnClientApp')
+  .controller("Chart", function ($scope, FilterService, DrawRabbitTempChartService, DrawKeyboardTempChartService) {
+ 
+    $scope.getSelectedSensor = function() {
+     if(document.getElementById('selected_sen'))
+     {
+        var vSkill = document.getElementById('selected_sen');
+        var vSkillText = vSkill.options[vSkill.selectedIndex].innerHTML;
+        if(vSkillText.localeCompare("temprabbit") == 0)
+          DrawRabbitTempChartService.GenerateChart();
+        else if(vSkillText.localeCompare("keyboard") == 0)
+          DrawKeyboardTempChartService.GenerateChart();
+      }
+    }
+
+    $scope.getChartOption = function() {
+     if(document.getElementById('chart_sel'))
+     {
+        var vSkill = document.getElementById('chart_sel');
+        var vSkillText = vSkill.options[vSkill.selectedIndex].innerHTML;
+        /*
+       if(vSkillText.localeCompare("Month Overview") == 0)
+          DrawChartService.GenetareChart("temprabbit");
+        else if(vSkillText.localeCompare("Single-Day Profile") == 0)
+          DrawChartService.GenetareChart1();
+        */
+      }
+    }
+  });
+
